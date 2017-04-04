0ad33056f1a5c0b809ddb5e86bef8aa6fdcd31a0
Mario <wysxhlyy@gmail.com>
2017-04-04 15:30:00 +0100

    Bug fix

--- app/src/main/java/com/example/mario/techinicianscheduler/DBHelper.java
+++ app/src/main/java/com/example/mario/techinicianscheduler/DBHelper.java
@@ -3,5 +3,5 @@ package com.example.mario.techinicianscheduler;
 public class DBHelper{
 
     //public static final String DB_ADDRESS="http://10.132.201.46/technicianScheduler/";
-    public static final String DB_ADDRESS="http://10.154.173.68/technicianScheduler/";
+    public static final String DB_ADDRESS="http://10.154.172.56/technicianScheduler/";
 }
